783c235b58c7aa23bc5a72d5c478232c8802a7ea
bureauverites <mahmodul2112@gmail.com>
2026-04-22 23:15:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+async function verifyCertificate() {
+
+    const input = document.getElementById("cert-number").value.trim();
+
+    if (!input) {
+        alert("Enter certificate number");
+        return;
+    }
+
+    const res = await fetch('./data.json');
+    const data = await res.json();
+
+    const found = data.find(item => item.id === input);
+
+    if (found) {
+
+        document.getElementById("search-section").classList.add("hidden");
+        document.getElementById("result-section").classList.remove("hidden");
+
+        document.getElementById("result-data").innerHTML = `
+            <div class="data-item"><span class="label">Deliverable Id :</span><br>${found.id}</div>
+            <div class="data-item"><span class="label">Published on :</span><br>${found.published}</div>
+            <div class="data-item"><span class="label">QR Code Status :</span><br><span style="color:green;">Validated</span></div>
+            <div class="data-item"><span class="label">NAME :</span><br>${found.name}</div>
+            <div class="data-item"><span class="label">ID :</span><br>${found.user_id}</div>
+            <div class="data-item"><span class="label">ISSUED ON :</span><br>${found.published}</div>
+            <div class="data-item"><span class="label">VALID UNTIL :</span><br>${found.valid_until}</div>
+            <div class="data-item"><span class="label">TYPE :</span><br>${found.type}</div>
+            <div class="data-item"><span class="label">MODEL :</span><br>N/A</div>
+            <div class="data-item"><span class="label">COMPANY :</span><br>PRIVATE</div>
+            <div class="data-item"><span class="label">TRAINING LOCATION :</span><br>JUBAIL</div>
+            <div class="data-item"><span class="label">TRAINER :</span><br>ZEESHAN KHAN</div>
+        `;
+    } else {
+        alert("Certificate not found");
+    }
+}
